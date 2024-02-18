00deb267baf92f1218736d1a2166ace912541f38
Benjamin T Jones <btjones16@gmail.com>
2024-02-18 10:02:55 -0500

    Updated install script to support Ubuntu.

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 
 echo "Installing the dependencies..."
 # Determine the operating system.
-if [ `uname`='Darwin' ];
+os_family=`uname`
+echo "Detected OS family " $os_family
+if [ $os_family = 'Darwin' ];
 then
+    echo "Installing for OSX"
     # If OS X, use Homebrew as a package manager.
     # Check if Homebrew is installed. If not, notify and install it.
     hash brew 2>/dev/null || {
@@ -15,10 +18,20 @@ then
     brew install boost
     read -n 1 -s -p "Installing GSL, press any key to continue or Ctrl-C to quit..."
     brew install gsl
-elif [ `uname`="Linux" ];
+elif [ $os_family = "Linux" ];
 then
-     echo "Support for this operating system is not yet available...aborting"
-     exit -1;     
+    echo "Installing for Linux"
+    dist=`lsb_release -d | awk -F"\t" '{print $2}' | awk -F" " '{print $1}'`
+    echo "Detected distribution " $dist
+    if [ $dist = "Ubuntu" ];
+    then
+	echo "Installing for Ubuntu"
+	echo "Installing build-essential libboost-dev, libgsl-dev, python3-dev, and swig."
+	sudo apt-get install build-essential libboost-dev libgsl-dev python3-dev swig
+    else
+	echo "Support for this operating system is not yet available...aborting"
+	exit -1;     
+    fi
 else
     echo "Unsupported operating system. Aborting."
     exit -1;
